fd54ab27d6975fa243e4a81763ccddb190c3636a
AslanNH <2464334025@qq.com>
2020-04-07 13:50:54 +0800

    activiti7

---
+++ chapter09_springconfig/spring_config_client/src/main/java/com/nh/App.java
@@ -0,0 +1,12 @@
+package com.nh;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class App {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(App.class).web(true).run(args);
+    }
+}
